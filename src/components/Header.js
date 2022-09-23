7de0eee3745f4a8004ae3ae162eeb5047f1fa59a
John Oblenda <joandong2@gmail.com>
2022-09-23 09:10:38 -0400

    working with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,45 @@ const Header = () => {
   return (
     <>
       <section id="header">
-        <div className="container mx-auto flex justify-between pt-4 mb-10">
+        <div className="bg-image"></div>
+        <div className="overlay"></div>
+        <div className="container mx-auto flex justify-between pt-4 mb-10 relative">
           <div>
-            <h1 className="font-dancing text-white text-4xl">estates</h1>
+            <h1 className="font-dancing text-white text-4xl">Real-estate</h1>
           </div>
           <div className="flex justify-between align-middle">
-            <ul className="flex space-x-4 align-middle mt-2">
+            <ul className="flex space-x-4 align-middle mt-4">
               <a href="/">Home</a>
               <a href="/">Listing</a>
               <a href="/">About</a>
               <a href="/">Contact Us</a>
             </ul>
-            <button className="ml-10 border border-solid px-2 py-2 rounded bg-blue-300 text-white">Create A Listing</button>
+            <button className="ml-10 border-solid text-white">Create A Listing</button>
           </div>
         </div>
-        <div className="container mx-auto flex flex-col space-y-4 text-white justify-center align-middle text-center mt-9">
-          <h1 className="text-6xl font-roboto">Welcome to Estates</h1>
+        <div className="container mx-auto flex flex-col space-y-4 text-white justify-center align-middle text-center mt-9 relative min-h-[200px]">
+          <h1 className="text-6xl font-dancing">Welcome to Real-estate</h1>
           <p>Bacon ipsum dolor amet ball tip chislic beef ribs shankle sirloin meatloaf. Flank tongue turducken chicken tail jerky. T-bone andouille leberkas, pig spare ribs buffalo tail doner landjaeger.Bacon ipsum dolor amet ball tip chislic beef ribs shankle sirloin meatloaf. Flank tongue turducken chicken tail jerky. T-bone andouille leberkas, pig spare ribs buffalo tail doner landjaeger.</p>
         </div>
-        <div className="overlay"></div>
+      </section>
+      <section id="hero" className="bg-red-400 pb-20">
+        <div className="columns-3 flex container mx-auto justify-center align-middle space-x-3"> 
+          <div className="hero-boxes p-4 bg-transparent text-white">
+            <h4>Easy to access</h4>
+            <p className="mb-4">Bacon ipsum dolor amet ball tip chislic beef ribs shankle sirloin meatloaf. Flank tongue turducken chicken tail jerky.</p>
+            <a href="/">Read More</a>
+          </div>
+          <div className="hero-boxes p-4 bg-transparent text-white">
+            <h4>Easy to access</h4>
+            <p className="mb-4">Bacon ipsum dolor amet ball tip chislic beef ribs shankle sirloin meatloaf. Flank tongue turducken chicken tail jerky.</p>
+            <a href="/">Read More</a>
+          </div>
+          <div className="hero-boxes p-4 bg-transparent text-white">
+          <h4>Easy to access</h4>
+            <p className="mb-4">Bacon ipsum dolor amet ball tip chislic beef ribs shankle sirloin meatloaf. Flank tongue turducken chicken tail jerky.</p>
+            <a href="/">Read More</a>
+          </div>
+        </div>
       </section>
     </>
   )
