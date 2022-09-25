d857a3a7add55c373275955568b6038bf4012450
John Oblenda <joandong2@gmail.com>
2022-09-24 21:13:29 -0400

    fixed tailwind issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ['./src/**/*.{html,js,tsx,ts}', './node_modules/tw-elements/dist/js/**/*.js'],
   plugins: [
     require('tw-elements/dist/plugin')
   ],
